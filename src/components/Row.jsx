a938580597c970dd32d800c4ed4f552a850af7af
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-02-21 18:40:56 +0900

    Added state toggling to buttons

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,7 +2,13 @@ import { v4 as uuid } from 'uuid';
 
 export default function Row({rowArray}) {
 
-  const hexTiles = rowArray.map((hex) => <button key={uuid()}>X:{hex[0]}<br/> Z:{hex[1]}</button>)
+  const hexTiles = rowArray.map((hex) => <button key={uuid()}  onClick={toggleActive}>X:{hex[0]}<br/> Z:{hex[1]}</button>)
+
+  function toggleActive(event) {
+    event.currentTarget.classList.toggle("btn-active");
+    console.log(event.currentTarget);
+
+  }
 
   return (
     <div>
