97fa1dc307b6108eff55cbebfe5e03122f156c23
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-02-22 17:50:12 +0900

    Added center hex marking

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,11 +2,19 @@ import { v4 as uuid } from 'uuid';
 
 export default function Row({rowId, rowArray}) {
 
+  function markCenterHexes(x, z) {
+    if ([0.23, 2.78, -1.05, 1.5].includes(x) && [1.16, -1.13].includes(z)) {
+      return "hex-button hex-board-center"
+    } else {
+      return "hex-button"
+    }
+  }
+
   const hexTiles = rowArray.map((hex, i) =>
     <button
       key={uuid()}
       data-hex={hex}
-      className="hex-button"
+      className={markCenterHexes(hex[0], hex[1])}
       onClick={toggleActive}>
         X:{hex[0]}<br/> Z:{hex[1]}
     </button>)
