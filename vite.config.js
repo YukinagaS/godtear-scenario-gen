f295d24c7cb859faf2a7cf43277714d8be261753
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-02-22 18:16:00 +0900

    Troubleshooting GHPages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/godtear-scenario-gen"
+  base: "/godtear-scenario-gen/"
 })
