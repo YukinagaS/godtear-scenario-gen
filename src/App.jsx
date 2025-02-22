acf7f8809e7f549290e0f6c92b7cdd62084bd506
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-02-22 16:20:50 +0900

    Added clear selection button

--- src/App.jsx
+++ src/App.jsx
@@ -10,50 +10,25 @@ function App() {
   // const [count, setCount] = useState(0)
   const boardRows = hexData.map((row) => <Row key={uuid()} rowArray={row} />)
 
+  function collectCoordinates() {
+    let returnArray = []
+    const allActive = document.querySelectorAll(".btn-active");
+    allActive.forEach((x) => returnArray.push(x.getAttribute("data-hex")));
+    console.log(returnArray);
+  }
+
+  function clearActive() {
+    const allActive = document.querySelectorAll(".btn-active");
+    allActive.forEach((element) => element.classList.remove("btn-active"));
+  }
+
   return (
       <main>
         {boardRows}
+        <button onClick={collectCoordinates}>Generate Array</button>
+        <button onClick={clearActive}>Clear Selections</button>
       </main>
   )
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-*/
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,12 +2,10 @@ import { v4 as uuid } from 'uuid';
 
 export default function Row({rowArray}) {
 
-  const hexTiles = rowArray.map((hex) => <button key={uuid()}  onClick={toggleActive}>X:{hex[0]}<br/> Z:{hex[1]}</button>)
+  const hexTiles = rowArray.map((hex) => <button key={uuid()}  data-hex={hex} onClick={toggleActive}>X:{hex[0]}<br/> Z:{hex[1]}</button>)
 
   function toggleActive(event) {
     event.currentTarget.classList.toggle("btn-active");
-    console.log(event.currentTarget);
-
   }
 
   return (
